9f355d8f06e44a2244a371811c3b55d7ea8edfaa
CrisA232534 <126267846+CrisA232534@users.noreply.github.com>
2024-11-19 23:12:34 -0500

    Update CODIGO_atm.cpp
    se agrego la clase cuuenta como seleccionador del menu con ka cuenta del usuario

--- CODIGO_atm.cpp
+++ CODIGO_atm.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
-
 using namespace std;
+// Clase
+class Cuenta {
+private:
+double saldo; // Saldo de la cuenta
+public:
+// Constructor 
+Cuenta(double saldoInicial) {
+saldo = saldoInicial;
+}
+// Método para depositar dinero
+    void depositar(double monto) {
+        if (monto > 0) {
+            saldo += monto;
+            cout << "Depósito exitoso. Nuevo saldo: $" << saldo << endl;
+        } else {
+            cout << "El monto debe ser positivo." << endl;
+        }
+    }
 
+    // Método para retirar dinero
+    void retirar(double monto) {
+        if (monto > 0 && monto <= saldo) {
+            saldo -= monto;
+            cout << "Retiro exitoso. Nuevo saldo: " << saldo << endl;
+        } else if (monto > saldo) {
+            cout << "Saldo insuficiente." << endl;
+        } else {
+            cout << "El monto debe ser positivo." << endl;
+        }
+    }
+
+    // Método para mostrar el saldo actual
+    void verSaldo() const {
+        cout << "Saldo actual: " << saldo << endl;
+    }
+};
 
 // Clase que controla el menu de inicio y compara el ingreso de datos con la base de datos
 class PantallaInicio {
