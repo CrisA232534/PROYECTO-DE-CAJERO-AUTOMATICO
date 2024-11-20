96d2bf277e1bd927a8f0b91bba159d5cfe8cdb9f
AleVilla07 <alejandro.villafuerte@ucb.edu.bo>
2024-11-20 00:07:41 -0400

    Update CODIGO_atm.cpp

--- CODIGO_atm.cpp
+++ CODIGO_atm.cpp
@@ -20,3 +20,24 @@ public:
     saldo_cajero = 10000;
     saldo_usuario[10] = ;
     }
+
+// Clase que controla el menu de inicio y compara el ingreso de datos con la base de datos
+class PantallaInicio {
+private:
+	//variables de tipo string para comparar texto en la base de datos
+    string nip;
+    string contrasena;
+
+public:
+    // Método que muestra el menu de inicio
+    void mostrarPantalla() {
+        cout<< "========================================\n";
+        cout<< "               BIENVENIDO!   \n";
+        cout<< "========================================\n";
+        cout<< "      escriba su numero de cuenta: \n";
+        cout<< "               ";
+        cin >> nip; // Leer el ID
+        cout<< "         escriba su contraseña: \n";
+        cout<< "               ";
+        cin >> contrasena; // Leer la contraseña
+    }
