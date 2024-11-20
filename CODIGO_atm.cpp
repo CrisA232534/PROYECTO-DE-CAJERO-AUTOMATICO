3c403a18e47c3f3527afa1aaa4dd4cbbae675a70
AleVilla07 <alejandro.villafuerte@ucb.edu.bo>
2024-11-20 00:08:17 -0400

    Update CODIGO_atm.cpp

--- CODIGO_atm.cpp
+++ CODIGO_atm.cpp
@@ -2,24 +2,6 @@
 
 using namespace std;
 
-// Definición de la clase
-class cajero {
-private:
-    int cuenta_usuarios[10];
-    int NIP_usuario[10];
-    int saldo_cajero;
-    int saldo_usuario[10];
-    
-
-public:
-    // Constructor
-    cajero(int _cuenta usuario[],int _NIP_usuario[],int _saldo_cajero,int _saldo Usuario[]) {
-        
-    cuenta_usuarios[10] =   ;
-    NIP_usuario[10] = ;
-    saldo_cajero = 10000;
-    saldo_usuario[10] = ;
-    }
 
 // Clase que controla el menu de inicio y compara el ingreso de datos con la base de datos
 class PantallaInicio {
